c854cc022bf6f0edcfeeb728caae96ec99e1106f
sultanfaqih <134555270+sultanfaqih@users.noreply.github.com>
2023-06-01 04:25:12 +0700

    Update menu1.sh

--- menu1.sh
+++ menu1.sh
@@ -21,7 +21,7 @@ trx=$(grep -c -E "^#! " "/etc/xray/config.json")
 ssx=$(grep -c -E "^## " "/etc/xray/config.json")
 #######PERIZINAN
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/MyRidwan/izinvps/ipuk/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/sultanfaqih/IP/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -39,7 +39,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/MyRidwan/izinvps/ipuk/ip | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/sultanfaqih/IP/main/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
